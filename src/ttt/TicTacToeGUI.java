ec33954b57e2050d4e673183ef83ffda5bbe004e
OnkelGPT <onkelgbt@gmail.com>
2025-07-01 10:44:40 +0200

    Add two player option and enlarge buttons

--- src/ttt/TicTacToeGUI.java
+++ src/ttt/TicTacToeGUI.java
@@ -11,12 +11,21 @@ import java.awt.event.ActionListener;
 public class TicTacToeGUI extends JFrame {
     private final JButton[] buttons = new JButton[9];
     private Board board = new Board();
-    private final char humanMark = 'X';
-    private final char aiMark = 'O';
-    private final MinimaxAI ai = new MinimaxAI(aiMark);
 
-    public TicTacToeGUI() {
+    // marks used by both game modes
+    private final char player1Mark = 'X';
+    private final char player2Mark = 'O';
+
+    private final MinimaxAI ai = new MinimaxAI(player2Mark);
+
+    /** true if the game is in two player mode */
+    private final boolean twoPlayer;
+
+    private char currentMark = player1Mark;
+
+    public TicTacToeGUI(boolean twoPlayer) {
         super("TicTacToe");
+        this.twoPlayer = twoPlayer;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(3, 3));
         initializeBoard();
@@ -30,6 +39,7 @@ public class TicTacToeGUI extends JFrame {
             JButton btn = new JButton(" ");
             final int index = i;
             btn.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
+            btn.setPreferredSize(new Dimension(100, 100));
             btn.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -43,14 +53,18 @@ public class TicTacToeGUI extends JFrame {
 
     private void handlePlayerMove(int index) {
         if (board.getCell(index) != ' ' || board.checkWin() != ' ') return;
-        board.placeMark(index, humanMark);
-        buttons[index].setText(String.valueOf(humanMark));
+
+        board.placeMark(index, currentMark);
+        buttons[index].setText(String.valueOf(currentMark));
         checkGameEnd();
-        if (board.checkWin() == ' ' && !board.isFull()) {
+
+        if (!twoPlayer && board.checkWin() == ' ' && !board.isFull()) {
             int move = ai.chooseMove(board);
-            board.placeMark(move, aiMark);
-            buttons[move].setText(String.valueOf(aiMark));
+            board.placeMark(move, player2Mark);
+            buttons[move].setText(String.valueOf(player2Mark));
             checkGameEnd();
+        } else {
+            currentMark = (currentMark == player1Mark) ? player2Mark : player1Mark;
         }
     }
 
@@ -58,12 +72,14 @@ public class TicTacToeGUI extends JFrame {
         char winner = board.checkWin();
         if (winner != ' ' || board.isFull()) {
             String message;
-            if (winner == humanMark) {
-                message = "You win!";
-            } else if (winner == aiMark) {
-                message = "Computer wins!";
-            } else {
+            if (winner == ' ') {
                 message = "Draw!";
+            } else if (twoPlayer) {
+                message = "Player " + winner + " wins!";
+            } else if (winner == player1Mark) {
+                message = "You win!";
+            } else {
+                message = "Computer wins!";
             }
             JOptionPane.showMessageDialog(this, message);
             resetGame();
@@ -75,13 +91,24 @@ public class TicTacToeGUI extends JFrame {
         for (int i = 0; i < 9; i++) {
             buttons[i].setText(" ");
         }
+        currentMark = player1Mark;
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new TicTacToeGUI().setVisible(true);
+                String[] options = {"Two Players", "Vs Computer"};
+                int choice = JOptionPane.showOptionDialog(null,
+                        "Choose game mode",
+                        "TicTacToe",
+                        JOptionPane.DEFAULT_OPTION,
+                        JOptionPane.INFORMATION_MESSAGE,
+                        null,
+                        options,
+                        options[0]);
+                boolean twoPlayer = choice == 0;
+                new TicTacToeGUI(twoPlayer).setVisible(true);
             }
         });
     }
